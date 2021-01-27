64fa4fa056781acd49bd81c89be7227a9e4b0ee7
redy <granredy@gmail.com>
2021-01-27 00:54:54 -0500

    limpieza de modo debug

--- src/script.js
+++ src/script.js
@@ -6,12 +6,12 @@
 	//Page cursors
 
     document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
-        t.style.left = n.clientX + "px", 
-		t.style.top = n.clientY + "px", 
-		e.style.left = n.clientX + "px", 
-		e.style.top = n.clientY + "px", 
-		i.style.left = n.clientX + "px", 
-		i.style.top = n.clientY + "px"
+  //       t.style.left = n.clientX + "px", 
+		// t.style.top = n.clientY + "px", 
+		// e.style.left = n.clientX + "px", 
+		// e.style.top = n.clientY + "px", 
+		// i.style.left = n.clientX + "px", 
+		// i.style.top = n.clientY + "px"
     });
     var t = document.getElementById("cursor"),
         e = document.getElementById("cursor2"),
